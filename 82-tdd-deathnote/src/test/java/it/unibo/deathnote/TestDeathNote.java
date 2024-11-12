21dd0e980a7c62d25da0a783af96819b7a14fe26
Davide Rossi - davide.rossi47@studio.unibo.it <davide.rossi47@studio.unibo.it>
2024-11-12 16:32:15 +0100

    Test negative or zero rule

--- 82-tdd-deathnote/src/test/java/it/unibo/deathnote/TestDeathNote.java
+++ 82-tdd-deathnote/src/test/java/it/unibo/deathnote/TestDeathNote.java
@@ -1,5 +1,48 @@
 package it.unibo.deathnote;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import it.unibo.deathnote.api.DeathNote;
+import it.unibo.deathnote.impl.DeathNoteImplementation;
+
 class TestDeathNote {
+
+    private DeathNote deathNote;
+
+    @BeforeEach
+    public void setUp(){
+        deathNote = new DeathNoteImplementation();
+    }
+
+
+    @Test
+    public void testGetNegativeOrZeroRule(){   
+        
+        try{
+            deathNote.getRule(0);
+            Assertions.fail("Getting rule number 0 should throw an error but it didn't");
+        }catch(Exception zeroRuleException){
+            assertEquals(IllegalArgumentException.class, zeroRuleException.getClass());
+            assertNotNull(zeroRuleException.getMessage());
+            assertFalse(zeroRuleException.getMessage().isBlank());
+        }
+
+        try{
+            deathNote.getRule(-1);
+            Assertions.fail("Getting a negative rule should throw an error but it didn't");
+        }catch(Exception negativeRuleException){
+            assertEquals(IllegalArgumentException.class, negativeRuleException.getClass());
+            assertNotNull(negativeRuleException.getMessage());
+            assertFalse(negativeRuleException.getMessage().isBlank());
+        }
+    }
+
+
 
 }
