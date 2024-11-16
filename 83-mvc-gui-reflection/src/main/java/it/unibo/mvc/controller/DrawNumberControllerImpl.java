c50c319651240a9f7e25ef72ee8b32e2aaedc6fb
Davide Rossi <davide.rossi47@studio.unibo.it>
2024-11-16 16:44:53 +0100

    Solved attempts decrease bug

--- 83-mvc-gui-reflection/src/main/java/it/unibo/mvc/controller/DrawNumberControllerImpl.java
+++ 83-mvc-gui-reflection/src/main/java/it/unibo/mvc/controller/DrawNumberControllerImpl.java
@@ -3,6 +3,7 @@ package it.unibo.mvc.controller;
 import it.unibo.mvc.api.DrawNumber;
 import it.unibo.mvc.api.DrawNumberController;
 import it.unibo.mvc.api.DrawNumberView;
+import it.unibo.mvc.api.DrawResult;
 
 import java.util.ArrayList;
 import java.util.Collection;
@@ -37,8 +38,9 @@ public final class DrawNumberControllerImpl implements DrawNumberController {
 
     @Override
     public void newAttempt(final int n) {
+        final DrawResult res = model.attempt(n);
         for (DrawNumberView view : views) {
-            Objects.requireNonNull(view, "There is no view attached!").result(model.attempt(n));   
+            Objects.requireNonNull(view, "There is no view attached!").result(res);   
         }
     }
 
