ab54734266b0672e76b189bce305fc9a94d95298
Davide Rossi <davide.rossi47@studio.unibo.it>
2024-11-16 16:41:13 +0100

    MultiView support with reflection

--- 83-mvc-gui-reflection/src/main/java/it/unibo/mvc/LaunchApp.java
+++ 83-mvc-gui-reflection/src/main/java/it/unibo/mvc/LaunchApp.java
@@ -1,6 +1,10 @@
 package it.unibo.mvc;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
 import it.unibo.mvc.api.DrawNumberController;
+import it.unibo.mvc.api.DrawNumberView;
 import it.unibo.mvc.controller.DrawNumberControllerImpl;
 import it.unibo.mvc.model.DrawNumberImpl;
 import it.unibo.mvc.view.DrawNumberConsoleView;
@@ -10,6 +14,8 @@ import it.unibo.mvc.view.DrawNumberSwingView;
  * Application entry-point.
  */
 public final class LaunchApp {
+
+    private static final int NVIEWS = 3;
 
     private LaunchApp() { }
 
@@ -24,11 +30,18 @@ public final class LaunchApp {
      * @throws IllegalAccessException in case of reflection issues
      * @throws IllegalArgumentException in case of reflection issues
      */
-    public static void main(final String... args) {
+    public static void main(final String... args) throws ClassNotFoundException, NoSuchMethodException,
+    InstantiationException, InvocationTargetException, IllegalAccessException{
         final var model = new DrawNumberImpl();
         final DrawNumberController app = new DrawNumberControllerImpl(model);
-        app.addView(new DrawNumberSwingView());
-        app.addView(new DrawNumberSwingView());
-        app.addView(new DrawNumberConsoleView());
+        final Class<?> swingViewClass = Class.forName("it.unibo.mvc.view.DrawNumberSwingView");
+        final Class<?> consoleViewClass = Class.forName("it.unibo.mvc.view.DrawNumberConsoleView");
+        final Constructor<?> swingViewConstructor = swingViewClass.getConstructor();
+        final Constructor<?> consoleViewConstructor = consoleViewClass.getConstructor();
+        for(int i = 0; i < NVIEWS; i++){
+            app.addView((DrawNumberView)swingViewConstructor.newInstance());
+            app.addView((DrawNumberView)consoleViewConstructor.newInstance());
+        }
+        
     }
 }
