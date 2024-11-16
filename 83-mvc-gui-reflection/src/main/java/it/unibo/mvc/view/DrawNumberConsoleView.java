c3613573cae9b036aa3df8aca905c64e76e398a4
Davide Rossi <davide.rossi47@studio.unibo.it>
2024-11-16 10:42:59 +0100

    Implement interface DrawNumberView in DrawNumberConsoleView

---
+++ 83-mvc-gui-reflection/src/main/java/it/unibo/mvc/view/DrawNumberConsoleView.java
@@ -0,0 +1,27 @@
+package it.unibo.mvc.view;
+
+import it.unibo.mvc.api.DrawNumberController;
+import it.unibo.mvc.api.DrawNumberView;
+import it.unibo.mvc.api.DrawResult;
+
+public class DrawNumberConsoleView implements DrawNumberView{
+
+    private static final String VIEW_NAME = "Output number console view";
+
+    @Override
+    public void setController(final DrawNumberController observer) {
+        return;
+    }
+
+    @Override
+    public void start() {
+        System.out.println(VIEW_NAME);
+    }
+
+    @Override
+    public void result(final DrawResult res) {
+        System.out.println(res.toString());
+    }
+    
+
+}
